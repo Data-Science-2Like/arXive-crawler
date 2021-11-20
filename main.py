ae80729d540e259304b6e1dc03a644bf54a50038
Simon Birkholz <simon.birkholz@uni-ulm.de>
2021-11-20 23:51:34 +0100

    wip

--- main.py
+++ main.py
@@ -1,23 +1,20 @@
 from oaipmh.client import Client
 from oaipmh.metadata import MetadataRegistry, oai_dc_reader
 
-def print_hi(name):
-
-    print(f'Hi, {name}')
+def get_records():
 
     URL = 'http://export.arxiv.org/oai2'
 
     registry = MetadataRegistry()
     registry.registerReader('oai_dc', oai_dc_reader)
+    registry
     client = Client(URL, registry)
 
-    for record in client.listRecords(metadataPrefix='oai_dc'):
-        print(record)
+    for (id,meta,_) in client.listRecords(metadataPrefix='oai_dc', set='cs'):
+        print(meta.getField('title'))
 
+    # for record in client.listRecords(metadataPrefix='oai_dc'):
+    #     print(record)
 
-
-# Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    get_records()
