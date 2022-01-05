826ad3cd6bb8175fb02cce88e64d443ba33d3e30
Simon Birkholz <simon.birkholz@uni-ulm.de>
2022-01-05 01:02:59 +0000

    fixes on server usage

--- dataCrawler.py
+++ dataCrawler.py
@@ -27,8 +27,14 @@ def get_proxy_list():
     # from https://stackoverflow.com/questions/48426624/scraping-free-proxy-listing-website
     resp = requests.get('https://free-proxy-list.net/')
     df = pd.read_html(resp.text)[0]
-    return df[(df['Https'] == 'yes') & (df['Country'] == 'Germany')]
+    return df[(df['Https'] == 'yes') & (df['Anonymity'] == 'elite proxy')]
 
+def get_proxy(proxy_list, use):
+    if not use:
+        return ""
+    idx = int(random() * len(proxy_list))
+    proxy_str = "http://" + str(proxy_list.iat[idx,0]) + ":" + str(proxy_list.iat[idx,1])
+    return proxy_str
 
 async def getData(dest, start_id, sleepLength, burstSize):
     # check if destination folder exists else create
@@ -40,6 +46,7 @@ async def getData(dest, start_id, sleepLength, burstSize):
     metaData = getPaperUrlsByCategory([10, 62, 122], start_id)
     requestCount = 0
     curr_proxy = str()
+    proxy_list = get_proxy_list()
     try:
         while requestCount < len(metaData):
             currUrls = list()
@@ -51,7 +58,7 @@ async def getData(dest, start_id, sleepLength, burstSize):
 
             newProxyNeeded = False
             async with aiohttp.ClientSession() as session:
-                ret = await asyncio.gather(*[downloadPaper(dest, url, id, session, curr_proxy) for (url, id) in currUrls])
+                ret = await asyncio.gather(*[downloadPaper(dest, url, id, session, get_proxy(proxy_list, len(curr_proxy) > 4)) for (url, id) in currUrls])
                 for status, st_url in ret:
                     if status is not Status.OK:
                         metaData.append(st_url)
@@ -66,7 +73,8 @@ async def getData(dest, start_id, sleepLength, burstSize):
                 proxy_list = get_proxy_list()
                 idx = int(random() * len(proxy_list))
                 curr_proxy = "http://" + str(proxy_list.iat[idx,0]) + str(":") + str(proxy_list.iat[idx,1])
-                print("Now using proxy ", curr_proxy)
+                # print("Now using proxy ", curr_proxy)
+                print("Got new proxy list")
     except BaseException as err:
         print("Error oucurred after ", requestCount, " requests with burstSize: ", burstSize, " and sleepLength: ",
               sleepLength)
@@ -91,7 +99,7 @@ async def downloadPaper(dest, url, identifier, session, proxy):
     # taken from https://stackoverflow.com/questions/57126286/fastest-parallel-requests-in-python
     try:
         if len(proxy) > 4:
-            async with session.get(url=url, proxy=proxy, max_redirects=10, allow_redirects=True) as response:
+            async with session.get(url=url, proxy=proxy, max_redirects=10, allow_redirects=True, timeout=10) as response:
                 resp = await response.read()
                 # First check if response was successfully
                 if response.status != 200:
