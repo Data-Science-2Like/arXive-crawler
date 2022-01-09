276ce0466fec41aae8a8f9316049f10b25602860
Simon B <simon.birkholz@uni-ulm.de>
2022-01-09 23:48:21 +0100

    fix: wrong stream operation

--- dataCrawler.py
+++ dataCrawler.py
@@ -43,7 +43,7 @@ def get_ids_from_disk(dest):
     for filename in os.listdir(dest):
         # now readin ids from json file
         if (filename.endswith("json")):
-            with open(os.path.join(dest, filename), 'w', encoding='utf-8') as f:
+            with open(os.path.join(dest, filename), 'r', encoding='utf-8') as f:
                 more_ids = json.load(f)
                 ids = ids + more_ids
                 continue
